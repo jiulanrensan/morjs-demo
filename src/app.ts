634b5123b8af60f0c06296e13f1253a34bd6a79e
20190225001 <839492148@qq.com>
2025-02-28 01:35:31 +0800

    feat: hack

--- src/app.ts
+++ src/app.ts
@@ -2,20 +2,36 @@
 // global.Symbol = Symbol
 // global.Map = Map
 // global.Set = Set
+
+function hackBehavior() {
+  global.Behavior = function (object) {
+    object.mixins = object.behaviors || [] 
+    object.behaviors = void 0
+    return Mixin(object)
+  }
+}
+
+function hackComponent() {
+  const origin = Component
+  Component = function (object) {
+    if (!object.options) object.options = {}
+    Object.assign(object.options, {
+      lifetimes: true
+    })
+    object.mixins = object.behaviors || []
+    object.behaviors = void 0
+    return origin.call(this, object)
+  }
+}
+hackBehavior()
+hackComponent()
+
 App<IAppOption>({
   globalData: {},
   onLaunch() {
+    wx.setStorageSync('logs', [1,2,3,4,5])
     // 展示本地存储能力
     const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        console.log(res.code)
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      },
-    })
+    console.log('logs', logs);
   },
 })
